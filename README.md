d7ae5f27d01b7c983461466d645e6ec83ac7cf11
Vladas <146100@gmail.com>
2024-06-06 13:26:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vents
+
+Events system.
---
+++ scripts/lint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eox pipefail
+
+rustup component add clippy
+
+cargo clippy \
+  -- \
+  \
+  -W clippy::all \
+  -W clippy::pedantic \
+  \
+  -A clippy::must-use-candidate \
+  -A clippy::missing-errors-doc \
+  -A clippy::missing-panics-doc \
+  -A clippy::module-name-repetitions \
+  -A clippy::return-self-not-must-use \
+  \
+  -D warnings
